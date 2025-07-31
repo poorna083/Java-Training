e1551517542adf1b537b0ad96c03686d84c4055f
poorna083 <dpoorna083@gmail.com>
2025-07-31 09:57:00 +0530

    I HAVE CHANGE SOMETHING

--- Day11/Demo.java
+++ Day11/Demo.java
@@ -1,19 +1,30 @@
-// Reversing the array
+//insert the element in the array 
+
+
 package Day11;
 import java.util.*;
-
 public class Demo {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
        Scanner sc = new Scanner(System.in);
        int size = sc.nextInt();  
        int[]array = new int[size];
-       for(int i=0;i<size;i++){
+       for(int i=0;i<size;i++)
+       {
         array[i] = sc.nextInt();
        }
-       for(int i=size-1;i>=0;i--){
-        System.out.print(array[i]+" ");
-       }
+        int new_array[] = new int[size+1];
+
+    for(int i=0;i<size;i++){
+        new_array[i] = array[i];
     }
-   
+    int new_element = sc.nextInt();
+    new_array[size]= new_element;
+    for(int i=0;i<=new_array.length;i++){
+        System.out.println(new_array[i]);
+    }
+   }
+
+}   
     
-}
+
