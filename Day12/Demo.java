940acefb6ebf96817dab9a2548e069f5a0f17959
poorna083 <dpoorna083@gmail.com>
2025-08-02 11:07:15 +0530

    new progeam has been inserted

--- Day12/Demo.java
+++ Day12/Demo.java
@@ -3,7 +3,7 @@ package Day12;
 
 import java.util.Scanner;
 
-public class Main {
+public class Demo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of elements: ");
