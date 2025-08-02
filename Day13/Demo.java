21debadad01c48d9a90e68079503de3b2296d126
poorna083 <dpoorna083@gmail.com>
2025-08-02 12:11:15 +0530

    new committed code

--- Day13/Demo.java
+++ Day13/Demo.java
@@ -1,28 +1,40 @@
 //wheather the element present in the array or not
 package Day13;
 import java.util.*;
-public class Demo {
-    public static void main(String[] args) {
+public class Demo 
+{
+//     public static void main(String[] args) 
+//     {
+//         Scanner sc = new Scanner(System.in);
+//         int size = sc.nextInt();
+//         int []array = new int[size];
+//         for(int i=0;i<size;i++)
+//         {
+//             array[i]=sc.nextInt();
+//         }
+//         Arrays.sort(array);
+//             System.out.println(array[size-1]);
+        
+//     }
+// }
+//the anoyher method
+
+public static void main(String[] args) 
+    {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
         int []array = new int[size];
-        for(int i=0;i<size;i++){
+        for(int i=0;i<size;i++)
+        {
             array[i]=sc.nextInt();
         }
-        int element = sc.nextInt();
-        boolean found = false;
-        for(int i=0;i<array.length;i++)
-        {
-                if(array[i]==element){
-                    System.out.println("element present");
-                    found = true;
-                    break;
-                }
-        } if(!found){
-                System.out.println("element not present");
-            }
+        int element = array[0];
         for(int i=0;i<array.length;i++){
-            System.out.println(array[i]);
-        }
+            if(array[i]>element){
+                element=array[i];
+            }
+        }System.out.println(element);
+        
+
     }
 }
