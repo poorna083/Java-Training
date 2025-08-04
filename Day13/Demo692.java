356673f80805e3c22efef555acd762f45f83bb69
poorna083 <dpoorna083@gmail.com>
2025-08-04 11:02:05 +0530

    new code inserted

---
+++ Day13/Demo692.java
@@ -0,0 +1,22 @@
+//transpose the matrix of an 2D array
+import java.util.*;
+public class Demo692 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int[][] array =new int[a][b];
+        for(int i=0;i<a;i++){
+            for(int j=0;j<b;j++){
+                array[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<a;i++){
+            for(int j=0;j<b;j++){
+                System.out.print(array[j][i]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
