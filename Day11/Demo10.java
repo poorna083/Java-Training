fef1dcc99b0f47460adc91dd30c71aa280fff12d
poorna083 <dpoorna083@gmail.com>
2025-08-04 18:06:10 +0530

    inserted

--- Day11/Demo10.java
+++ Day11/Demo10.java
@@ -1,4 +1,27 @@
+//Sum of each Row of the matrix
 package Day11;
-public class Demo10 {
-    
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("row_val:");
+        int row = sc.nextInt();
+         System.out.println("colum_val:");
+        int colum = sc.nextInt();
+        System.out.println("enter the array value of "+ row + "x" + colum);
+        int [][]array=new int[row][colum];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<colum;j++){
+                array[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0;i<row;i++){
+            int sum = 0;
+            for(int j=0;j<colum;j++){
+                sum = sum+array[i][j];
+            }System.out.println("sum of the rows is:"+i+" "+sum);
+        }
+    }
 }
