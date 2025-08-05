2365e63c11eafbda72808f3a7ad27cc4619a494a
poorna083 <dpoorna083@gmail.com>
2025-08-05 21:57:05 +0530

    inserted

--- Day12/Demo6.java
+++ Day12/Demo6.java
@@ -2,17 +2,18 @@
 package Day12;
 import java.util.*;
 public class Demo6{
-       int a = sc.nextInt();
-      for(int i=1;i<=a;i++){
-              b = a*i;
-      }
-      System.out.println(b);
+    static void main(int a){
+        int b=1;
+        for(int i=1;i<=a;i++){
+             b = b*i;
+            System.out.println(b);
+        }
     }
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        main(c);
+    }
 }
-
-
 
 
