55bbb7ff56608bc5503a2be54ca25617eee068c5
poorna083 <dpoorna083@gmail.com>
2025-08-05 19:37:56 +0530

    new code inserted

---
+++ Day12/Demo8.java
@@ -0,0 +1,9 @@
+package Day12;
+import java.util.*;
+public class Demo8 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String str = sc.nextLine();
+    
+    }
+}
