0cba2ea6eb903c4e68f6974a8a4d7eaab8f3fc26
poorna083 <dpoorna083@gmail.com>
2025-08-07 11:58:19 +0530

    array list

---
+++ Day14/Demo.java
@@ -0,0 +1,16 @@
+//array list 
+package Day14;
+import java.util.*;
+public class Demo {
+    public static void main(String[] args) {
+        List <String> a = new ArrayList<>();
+        a.add("aa");
+        a.add("bb");
+        a.add("cc");
+        a.add("dd");
+        for(String i:a){
+            System.out.println(i);
+        }
+
+    }
+}
